6392ab3aa7f936f03a6d8a4d42534616657e6bea
nekmo <nekmo@alsur.es>
2016-10-31 16:33:38 +0100

    Deleted model use in context processors.

--- cms_bs3_theme/context_processors.py
+++ cms_bs3_theme/context_processors.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from cms_bs3_theme.models import ThemeSite
+# from cms_bs3_theme.models import ThemeSite
 
 
 def settings(request):
@@ -7,7 +7,7 @@ def settings(request):
     """
     from . import conf
     conf = dict(vars(conf))
-    conf.update(ThemeSite.objects.get_theme_conf(request=request, fail=False))
+    # conf.update(ThemeSite.objects.get_theme_conf(request=request, fail=False))
     data = request.session.get('cms_bs3_theme_conf', {})
     conf.update(data)
     return {'bs3_conf': conf}
