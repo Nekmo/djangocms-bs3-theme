0949a3766a0a2c7a702ad605155b5246d4058a88
Nekmo <contacto@nekmo.com>
2019-08-20 01:09:39 +0200

    Let's Encrypt host envs.

--- demo/demo_app/settings/production.py
+++ demo/demo_app/settings/production.py
@@ -6,7 +6,7 @@ DATABASES = {
     'default': {
         'CONN_MAX_AGE': 0,
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'postgres',
+        'HOST': os.getenv("POSTGRES_HOST", 'postgres'),
         'NAME': os.getenv("POSTGRES_DB"),
         'USER': os.getenv("POSTGRES_USER"),
         'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
